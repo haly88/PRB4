d9815aeec12b6f4fa822e15a512604dfd7091a76
Haly88 <ramiromusso@gmail.com>
2015-04-14 23:45:43 -0300

    gravatar images

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,8 @@
 module ApplicationHelper
 
 	def avatar_url(usuario)
-		default_url = "#{root_url}public/mcpato.png"
+		default_url = "#{root_url}mcpato.png"
 		gravatar_id = Digest::MD5.hexdigest(usuario.email.downcase)
-		"http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{CGI.escape(default_url)}"
-		binding.pry
+		"http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=mm"
 	end
 end
